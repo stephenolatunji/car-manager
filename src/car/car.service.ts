5a8cc790487beb361be8917d0a3116bfb0db7fb9
Stephen <stephenolatunji02@gmail.com>
2022-10-15 23:59:58 +0100

    Completed car.service.ts

--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
 import { CARS } from './cars.mock';
 
 @Injectable()
@@ -7,17 +7,23 @@ export class CarService {
     public getCars(){
         return this.cars;
     }
-    public getCarById(id: Number){
+    public getCarById(id: number){
         const car =  this.cars.find(car => car.id === id);
+        if(!car) throw new HttpException('Not Found!', 404);
         return car;
     }
     public postCar(car){
         return this.cars.push(car);
     }
-    public putCarById(id){
+    public putCarById(id: number, propertyName: string, propertyValue: string){
+        const carIndex = this.cars.findIndex(car => car.id === id);
+        if(carIndex === -1) throw new HttpException('Not Found!', 404);
+        this.cars[carIndex][propertyName] = propertyValue;
         return 
     }
-    public deleteCarById(id){
-        return
+    public deleteCarById(id: number){
+        const carIndex = this.cars.findIndex(car => car.id === id)
+        if(carIndex === -1) throw new HttpException('Not Found!', 404);
+        return this.cars.splice(carIndex, 1)
     }
 }
